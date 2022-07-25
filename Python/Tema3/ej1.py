020f88b0c86ae2e7394dd924c6943fa7f220519a
Esteban5957L <4estebanvillalba20@gmail.com>
2022-07-25 17:19:08 -0300

    tema3

---
+++ Python/Tema3/ej1.py
@@ -0,0 +1,21 @@
+Nombre: str = "Esteban"
+Apellidos: str= "Villalba"
+Edad: int = 23
+Email: str = "email@gmail.com"
+Teléfono: int = 12345
+Casado = False
+Hijos = False
+amigos = ("lola", "mono")
+Películas = {
+    "clave": "nombrePelicula",
+        
+}
+print(Nombre)
+print(Apellidos)
+print(Edad)
+print(Email)
+print(Teléfono)
+print(Casado)
+print(Hijos)
+print(amigos)
+print(Películas)
